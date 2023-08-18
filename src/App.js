4f2df6bb32da1afed10526f99afdf173512dee02
Javiov <javieroviedo0512@gmail.com>
2023-08-18 11:56:33 -0300

    child

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Ejemplo2 from './hooks/Ejemplo2';
 import MiComponenteConContexto  from './hooks/Ejemplo3';
 import Ejemplo4 from './hooks/Ejemplo4';
 import GreetingStyled from './components/pure/greetingStyled';
+import Father from './components/container/father';
 
 function App() {
   return (
@@ -26,7 +27,8 @@ function App() {
       {/* <GreetingStyled name= "Javier"></GreetingStyled>*/}
      
      {/* </header> */}
-     <TaskListComponent></TaskListComponent>
+    {/* <TaskListComponent></TaskListComponent>*/}
+    <Father></Father>
  </div> 
  );
 }
--- src/components/pure/taskForm.jsx
+++ src/components/pure/taskForm.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const TaskForm = () => {
   return (
     <div>
-      
+      {/* */}
     </div>
   );
 }
