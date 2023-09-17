e6810ddade8c161555f598af016b35d1394bcb07
Javiov <javieroviedo0512@gmail.com>
2023-09-17 17:44:54 -0300

    fe

---
+++ src/AppRoutingOne.js
@@ -0,0 +1,15 @@
+
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import HomePage from './home/HomePage';
+import NotFoundPage from './pages/404/NotFoundPage';
+
+
+function AppRoutingOne() {
+  return (
+    <div className="App">
+  <Router></Router>
+</div> 
+ );
+}
+
+export default AppRoutingOne;
--- src/home/HomePage.jsx
+++ src/home/HomePage.jsx
@@ -4,7 +4,7 @@ const HomePage = () => {
     return (
         <div>
             <h1>Home Page</h1>
-            <h2>DashBoard</h2>
+           
         </div>
     );
 }
